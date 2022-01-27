b24bb82f43ad97372425c1d112a83e6e2456646a
shiwei23 <shiweiliu23@gmail.com>
2022-01-27 16:07:42 -0500

    merfish new data analysis

---
+++ run_scripts/20210717-MOp_crick_full.bat
@@ -0,0 +1,13 @@
+call activate merlin_env
+merlin -a 20210717-MOp_full.json ^
+-o M1_38bits.csv ^
+-c M1_codebook.csv ^
+-m storm6_microscope.json ^
+-p 20210717-positions_all.txt ^
+-e L:\Shiwei\Data ^
+-s L:\Shiwei\Analysis ^
+-n 12 ^
+--no_report True ^
+20210717MO4
+
+
